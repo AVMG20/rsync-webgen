32dcb1b7c7a892cb704fe840a6db1165349aff14
AVMG <rijstenpap1234@hotmail.com>
2023-07-12 20:39:43 +0200

    added node server and enabled prerender

---
+++ server.js
@@ -0,0 +1,18 @@
+import { handler } from './build/handler.js';
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'))
+
+// add a route that lives separately from the SvelteKit app
+app.get('/healthcheck', (req, res) => {
+    res.end('ok');
+});
+
+// let SvelteKit handle everything else, including serving prerendered pages and static assets
+app.use(handler);
+
+app.listen(3001, () => {
+    console.log('listening on port 3001');
+});
